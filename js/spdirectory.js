211a0a687dff9e117b518a8111a4b27c70fc993e
dhardin <dhardin@ltu.edu>
2014-07-07 14:32:02 -0400

    Module now pulls list data as well.  Temporarily disabled jstree plugin for testing purposes.  Plain vanilla unsorted list ;)

--- js/spdirectory.js
+++ js/spdirectory.js
@@ -20,13 +20,20 @@ var spdirectory = (function () {
                 url: true
             },
             tree_item_map : {
-                parent: String()
-                    + '<ul>'
+                pageParentList: String()
+                    + '<ul class="parentPage">'
                         + '<li>'
-                            + '<a></a>'
-                            + '<ul></ul>'
+                            + '<a>Pages</a>'
+                            + '<ul class="pages"></ul>'
                         + '</li>'
                     + '</ul>',
+                listParentList: String()
+                  + '<ul class="parentList">'
+                      + '<li>'
+                          + '<a>Lists</a>'
+                          + '<ul class="lists"></ul>'
+                      + '</li>'
+                  + '</ul>',
                 child: '<li><a></a></li>'
             },
             data_tree_map : {//data-jstree=
@@ -73,7 +80,7 @@ var spdirectory = (function () {
 
                     title = $this.title;
                     url = $this.getAttribute('url');
-                    results.push({ title: title, url: url });
+                    results.push({ title: title, url: url, type: 'web' });
                 });
 
                 if (callback) {
@@ -82,13 +89,42 @@ var spdirectory = (function () {
             },
             contentType: "text/xml; charset=\"utf-8\""
         });
-        //get list of webs from webservice
-            //callback - iterate through each web and create object and append to web arr
     };
 
-    getLists = function (url, callback) {
-        var listArr = [];
-        return listArr;
+    getLists = function (url, $target, callback) {
+        var results = [],
+
+        // Create the SOAP request
+         soapEnv =
+            '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">\
+                <soap:Body>\
+                  <GetListCollection xmlns="http://schemas.microsoft.com/sharepoint/soap/" />\
+                </soap:Body>\
+            </soap:Envelope>';
+
+
+        $.ajax({
+            url: url + "/_vti_bin/lists.asmx",
+            type: "POST",
+            dataType: "xml",
+            data: soapEnv,
+            error: printError,
+            complete: function (xData, status) {
+                $(xData.responseText).find("list").each(function () {
+                    var $this = $(this)[0],
+                    title, url;
+
+                    title = $this.title;
+                    url = $this.getAttribute('DefaultViewUrl');
+                    results.push({ title: title, url: url, type: 'list' });
+                });
+
+                if (callback) {
+                    callback(results, $target);
+                }
+            },
+            contentType: "text/xml; charset=\"utf-8\""
+        });
     };
 
     printError = function (XMLHttpRequest, textStatus, errorThrown) {
@@ -96,33 +132,53 @@ var spdirectory = (function () {
         console.log(XMLHttpRequest.responseText);
     };
 
-    processResult = function (arr, $target) {
+    processResult = function (arr, $target, type) {
         var i;
         //var i,
         //    targetTitle = options.targetTitle || "",
         //    targetUrl = options.targetUrl || "",
         //    targetIcon = options.targetIcon || "";
         if (arr.length == 0 && $target.find('ul')) {
-            $target.find('ul').remove();
+            if (type == 'list') {
+                $target.find('.parentPage').remove();
+            }
+            else {
+                $target.find('.parentList').remove();
+            }
         }
 
         for (i = 0; i < arr.length; i++){
             var $listItem = $(configMap.tree_item_map.child),
-                $childList = $('<ul/>');
+                $pageChildList = $(configMap.tree_item_map.pageParentList),
+                $listChildList = $(configMap.tree_item_map.listParentList);
 
-            $childList.appendTo($listItem);
+           
 
             $listItem.find('a').text(arr[i].title);
-            $listItem.find('a').attr('href', arr[i].url); 
-            $listItem.attr('data-jstree', configMap.data_tree_map.page);
+            $listItem.find('a').attr('href', arr[i].url);
+           
+            $listItem.attr('data-jstree', arr[i].type == 'web' ? configMap.data_tree_map.page : configMap.data_tree_map.list);
             $listItem.appendTo($target);
             
 
-            getWebs(arr[i].url, $childList, function (results, $targetList) {
-                processResult(results, $targetList);
-            });
+           
+
+            if (type == 'web') {
+                $pageChildList.appendTo($listItem);
+                $listChildList.appendTo($listItem);
+
+                getLists(arr[i].url, $listChildList.find('.lists'), function (results, $targetList) {
+                    processResult(results, $targetList, 'list');
+                });
+
+                getWebs(arr[i].url, $pageChildList.find('.pages'), function (results, $targetList) {
+                    processResult(results, $targetList, 'web');
+               
+                });
+            }
         }
     };
+
     //----------------- END UTILITY METHODS ----------------------
     //--------------------- BEGIN DOM METHODS --------------------
     // Begin DOM method /setJqueryMap/
@@ -158,12 +214,13 @@ var spdirectory = (function () {
 
         setJqueryMap();
         getWebs(settings_map.url, jqueryMap.$treeTop, function (results, $target) {
-            processResult(results, $target);
+            processResult(results, $target, 'web');
         });
 
         //initiate tree
-      
-
+        //setTimeout(function () {
+        //    jqueryMap.$container.jstree();
+        //}, 10000);
         jqueryMap.$tree
            // listen for event
            .on('changed.jstree', function (e, data) {
